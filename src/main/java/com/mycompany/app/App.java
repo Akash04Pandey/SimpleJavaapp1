7bf749c9d2fe24c95cff8f2d0d77bea7656a9730
Akash04Pandey <95336459+Akash04Pandey@users.noreply.github.com>
2021-12-12 20:18:49 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("squre root");
-        System.out.println("hello 2");
+        System.out.println( "Hello Dev-Ops" );
     }
 }
